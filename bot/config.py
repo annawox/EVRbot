c2add39cf8054367605df13df00f21f10c815917
anna <143606587+annawox@users.noreply.github.com>
2024-12-01 07:59:51 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 # Загружаем переменные из файла .env
 load_dotenv()
 
-API_TOKEN = os.getenv("API_TOKEN") !!!!!!!!!!!!!!!!!
+API_TOKEN = os.getenv("8043599454:AAHIhhr8Z3NDlmu7sRiP2H83e_3F9ZSHapo")
