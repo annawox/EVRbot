ba1c8989808f76413ade4fb29eb3f36ef4a3f450
anna <143606587+annawox@users.noreply.github.com>
2024-12-01 07:26:47 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
-bot = Bot(token="YOUR_TOKEN")
+bot = Bot(token="YOUR_TOKEN") !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
